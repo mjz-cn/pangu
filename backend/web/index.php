6fc0df0275919251ee10dfdf88d746973984a809
majunzhe <junzhe.ma@xinmei365.com>
2017-11-06 13:22:18 +0800

    add config local

--- backend/web/index.php
+++ backend/web/index.php
@@ -2,7 +2,6 @@
 
 define('YII_ENV', 'prod');
 
-
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
